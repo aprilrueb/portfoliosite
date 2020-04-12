7e88c0189f3a8c0ae9e9c911c7adcf1ef27a9356
April Rueb <aprilrueb@gmail.com>
2020-04-12 02:08:47 -0400

    Updates

---
+++ precache-manifest.6bc9dc890a9da970c7d3a2664968e6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ae93a4d654a01e0234ee4bd3a01d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e13b8cd38aaedc2d137",
+    "url": "/static/css/main.d3365973.chunk.css"
+  },
+  {
+    "revision": "288972efae1ed3c945f7",
+    "url": "/static/js/2.70e0ce54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70e0ce54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e13b8cd38aaedc2d137",
+    "url": "/static/js/main.7dcab92c.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
